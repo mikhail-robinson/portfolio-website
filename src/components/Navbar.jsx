1c683d6f37801ef6bd48ed0f83682caa6dd386c4
Mikhail Robinson <mikhailrobinson0@gmail.com>
2023-07-13 16:35:29 +1200

    Add responsiveness to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FaBars, FaTimes } from 'react-icons/fa'
 import Profile from '../assets/profile.jpg'
 const Navbar = () => {
+  const [nav, setNav] = useState(false)
+  const handleClick = () => setNav(!nav)
+
   return (
     <div className="fixed w-full h-{80px} flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
       <div>
@@ -12,23 +15,8 @@ const Navbar = () => {
         />
       </div>
       {/* menu */}
-      <div>
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Skills</li>
-          <li>Work</li>
-          <li>Contact</li>
-        </ul>
-      </div>
 
-      {/* Hamburger */}
-      <div className="hidden">
-        <FaBars />
-      </div>
-
-      {/* Mobile Menu */}
-      <ul className="hidden">
+      <ul className="hidden md:flex">
         <li>Home</li>
         <li>About</li>
         <li>Skills</li>
@@ -36,8 +24,28 @@ const Navbar = () => {
         <li>Contact</li>
       </ul>
 
+      {/* Hamburger */}
+      <div onClick={handleClick} className="md:hidden z-10">
+        {!nav ? <FaBars /> : <FaTimes />}
+      </div>
+
+      {/* Mobile Menu */}
+      <ul
+        className={
+          !nav
+            ? 'hidden'
+            : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'
+        }
+      >
+        <li className="py-6 text-4xl">Home</li>
+        <li className="py-6 text-4xl">About</li>
+        <li className="py-6 text-4xl">Skills</li>
+        <li className="py-6 text-4xl">Work</li>
+        <li className="py-6 text-4xl">Contact</li>
+      </ul>
+
       {/* Social Icons */}
-      <div></div>
+      <div className="hidden"></div>
     </div>
   )
 }
