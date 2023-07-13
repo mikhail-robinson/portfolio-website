55f3e3a505ece78dad47dc1181c674f3711b9f06
Mikhail Robinson <mikhailrobinson0@gmail.com>
2023-07-13 17:20:58 +1200

    start creating home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Home from './components/Home'
 import Navbar from './components/Navbar'
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Home />
       <h1 className="font-bold text-2xl">Hello</h1>
     </div>
   )
