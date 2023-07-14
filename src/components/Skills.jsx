4cde141136b7d957556305441d20679467bc1901
Mikhail Robinson <mikhailrobinson0@gmail.com>
2023-07-14 18:01:07 +1200

    Finish Skills component

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -2,27 +2,65 @@ import React from 'react'
 
 import HTML from '../assets/html.png'
 import CSS from '../assets/css.png'
+import Javascript from '../assets/javascript.png'
 import Typescript from '../assets/typescript.png'
 import ReactImg from '../assets/react.png'
 import Github from '../assets/github.png'
 import Redux from '../assets/redux.png'
+import Sqlite from '../assets/sqlite.png'
 
 const Skills = () => {
   return (
-    <div name="skills" className=" bg-[#0a192f] text-gray-300">
+    <div name="skills" className="w-full h-screen bg-[#0a192f] text-gray-300">
       {/* Container */}
-      <div className="max-2-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
         <div>
           <p className="text-4xl font-bold inline border-b-4 border-pink-600">
-            Experience
+            Skills
           </p>
           <p className="py-4">These are the technologies i've worked with</p>
         </div>
 
-        <div className="w-full grid grid-cols-2 sm:grid-cols-6 gap-4 text-center py-8">
+        <div className="w-full grid md:grid-cols-4 sm:grid-cols-2 gap-4 text-center py-8">
           <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
             <img className="w-20 mx-auto" src={HTML} alt="HTML Logo" />
             <p className="my-4">HTML</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={CSS} alt="CSS Logo" />
+            <p className="my-4">CSS</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img
+              className="w-20 mx-auto"
+              src={Javascript}
+              alt="Javascript Logo"
+            />
+            <p className="my-4">Javascript</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img
+              className="w-20 mx-auto"
+              src={Typescript}
+              alt="Typescript Logo"
+            />
+            <p className="my-4">Typescript</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={ReactImg} alt="React Logo" />
+            <p className="my-4">React</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={Github} alt="Github Logo" />
+            <p className="my-4">Github</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={Redux} alt="Redux Logo" />
+            <p className="my-4">Redux</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={Sqlite} alt="SQLite Logo" />
+            <p className="my-4">SQLite</p>
           </div>
         </div>
       </div>
