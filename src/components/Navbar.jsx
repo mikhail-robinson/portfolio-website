e46b73f2e94ee55825426850f0d4e3d4c60ac45d
Mikhail Robinson <mikhailrobinson0@gmail.com>
2023-07-14 20:03:21 +1200

    Add link to CV

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -121,7 +121,7 @@ const Navbar = () => {
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]">
             <a
               className="flex justify-between items-center w-full text-gray-300"
-              href="/"
+              href="https://drive.google.com/file/d/11iWfBoO_sUvO2aiCxUcsk_vBrNGdAyws/view?usp=sharing"
             >
               CV <BsFillPersonLinesFill size={30} />
             </a>
