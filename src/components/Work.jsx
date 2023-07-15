526a1949b5865f1f2c8b8a39069a0fe02add5627
Mikhail Robinson <mikhailrobinson0@gmail.com>
2023-07-15 13:35:42 +1200

    update demo link

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -24,7 +24,7 @@ const Work = () => {
                 Pinky Promise
               </span>
               <div className="pt-8 text-center">
-                <a href="/">
+                <a href="https://youtu.be/rbBYRWjBm-o">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Demo
                   </button>
