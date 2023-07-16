c8c8ac70b367fba9d4cfbdc4eb44a4c5f9e00e8c
Mikhail Robinson <mikhailrobinson0@gmail.com>
2023-07-17 09:53:22 +1200

    Fix text being covered by navbar

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -11,9 +11,12 @@ import Sqlite from '../assets/sqlite.png'
 
 const Skills = () => {
   return (
-    <div name="skills" className="w-full h-screen bg-[#0a192f] text-gray-300">
+    <div
+      name="skills"
+      className="w-full min-h-screen bg-[#0a192f] text-gray-300"
+    >
       {/* Container */}
-      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full min-h-full pt-40">
         <div>
           <p className="text-4xl font-bold inline border-b-4 border-pink-600">
             Skills
