4dd798806944938a64e2b5839edc31a60ef7486f
Mikhail Robinson <mikhailrobinson0@gmail.com>
2023-07-17 16:13:18 +1200

    Add additional projects to work

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import Pinky from '../assets/pinky.png'
+import Website from '../assets/website.png'
+import Widget from '../assets/widget.png'
+import Progress from '../assets/progress.jpg'
 
 const Work = () => {
   return (
@@ -11,8 +14,8 @@ const Work = () => {
           </p>
           <p className="py-6">Check out some of my recent work</p>
         </div>
-
-        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
+        {/* Container */}
+        <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
           {/* Grid Items */}
           <div
             style={{ backgroundImage: `url(${Pinky})` }}
@@ -30,6 +33,78 @@ const Work = () => {
                   </button>
                 </a>
                 <a href="https://github.com/mikhail-robinson/pinky-promise">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          {/* Grid Items */}
+          <div
+            style={{ backgroundImage: `url(${Website})` }}
+            className="shadow-lg shadow-[#040316] group container rounded-md flex justify-center items-center mx-auto content-div "
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                This Website!
+              </span>
+              <div className="pt-8 text-center">
+                <a href="/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a href="https://github.com/mikhail-robinson/portfolio-website">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          {/* Grid Items */}
+          <div
+            style={{ backgroundImage: `url(${Widget})` }}
+            className="shadow-lg shadow-[#040316] group container rounded-md flex justify-center items-center mx-auto content-div "
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                Widgets!
+              </span>
+              <div className="pt-8 text-center">
+                <a href="https://mikhailr-widgets.devacademy.nz/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a href="https://github.com/mikhail-robinson/widgets-full-stack">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          {/* Grid Items */}
+          <div
+            style={{ backgroundImage: `url(${Progress})` }}
+            className="shadow-lg shadow-[#040316] group container rounded-md flex justify-center items-center mx-auto content-div "
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                Study Zone IN PROGRESS
+              </span>
+              <div className="pt-8 text-center">
+                <a href="/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a href="/">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Code
                   </button>
